ab9e7aa726c0c6664dcc6fe67f6b83bdb5f4256f
EmirOcalan <emirocalan58@gmail.com>
2023-03-26 18:28:30 +0300

    feat: add user inputs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hesap Makinesi");
+
+            Console.Write("Birinci Sayıyı Giriniz: ");
+            int sayi1 = int.Parse(Console.ReadLine());
+            
+            Console.Write("İkinci Sayıyı Giriniz: ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+
+
         }
     }
 }
