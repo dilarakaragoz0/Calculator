a7bb7cbb819b018100438086ab4385cf46cb23c2
EmirOcalan <emirocalan58@gmail.com>
2023-03-26 19:12:39 +0300

    refactor: some minor code clean

--- Program.cs
+++ Program.cs
@@ -14,25 +14,17 @@
 
             Console.Write("İkinci Sayıyı Giriniz: ");
             double sayi2 = double.Parse(Console.ReadLine());
+
             double sonuc = 0;
             switch (islem)
             {
-                case "+":
-                    sonuc = sayi1 + sayi2;
-                    break;
-                case "-":
-                    sonuc = sayi1 - sayi2;
-                    break;
-                case "*":
-                    sonuc = sayi1 * sayi2;
-                    break;
-                case "/":
-                    sonuc = sayi1 / sayi2;
-                    break;
-                case "%":
-                    sonuc = sayi1 % sayi2;
-                    break;
+                case "+":sonuc = sayi1 + sayi2;break;     
+                case "-":sonuc = sayi1 - sayi2;break;    
+                case "*":sonuc = sayi1 * sayi2;break;    
+                case "/":sonuc = sayi1 / sayi2;break;
+                case "%":sonuc = sayi1 % sayi2;break;        
             }
+
             Console.WriteLine(sayi1 + " " + islem + " " + sayi2 + " = " + sonuc);
         }
     }
